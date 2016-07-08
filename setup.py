8b0b0751e5f9c16155241611db03fffd982f764d
Chris Calderon <calderon.christian760@gmail.com>
2016-07-08 16:26:28 -0700

    Switched to universal setup.py + config.json

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
-#!/usr/bin/env python
 from setuptools import setup
+import json
+
+with open('config.json') as cfg:
+    config = json.load(cfg)
+
+
+def is_http(req):
+    """Checks if a requirement is a link."""
+    return req.startswith('http://') or req.startswith('https://')
+
+
+def dep_sorter(rs, r):
+    """Appends the requirement r to the proper list."""
+    print rs, r
+    if is_http(r):
+        rs[1].append(r)
+    else:
+        rs[0].append(r)
+    return rs
 
 with open('requirements.txt') as reqs:
-    requirements = filter(lambda s: s!='',
-                          map(lambda s: s.strip(), reqs))
+    requirements, links = reduce(dep_sorter,
+                                 filter(None,
+                                        [r.strip() for r in reqs]),
+                                 [[],[]])
 
-setup(name='LoadContracts',
-      version='1.0',
-      description='Uploads Serpent Dapps onto the Ethereum network',
-      author='ChrisCalderon',
-      author_email='calderon.christian760@gmail.com',
-      license='MIT',
-      packages=['load_contracts'],
-      package_data={'load_contracts':['*.se']},
-      install_requires=requirements)
+config['install_requires'] = requirements
+config['dependency_links'] = links
+setup(**config)
